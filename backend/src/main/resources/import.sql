c2b521f939780eb93f6e062adf91ccec0cdb48fb
Roberto Costa <robertocosta66@gmail.com>
2024-04-13 11:14:16 -0300

    Enrollment, EnrollmentPK

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -30,3 +30,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id )
 INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capitulo 1','Neste capítulo vamos começar',1,'https://www.picpedia.org/suspension-file/images/online-courses.jpg',1,null);
 INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capitulo 2','Neste capítulo vamos continuar',2,'https://www.picpedia.org/suspension-file/images/online-courses.jpg',1,1);
 INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capitulo 3','Neste capítulo vamos finalizar',3,'https://www.picpedia.org/suspension-file/images/online-courses.jpg',1,2);
+
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_moment, refund_moment, available, only_update) VALUES(1,1,TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z',null, true, false)
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_moment, refund_moment, available, only_update) VALUES(2,1,TIMESTAMP WITH TIME ZONE '2020-11-21T11:00:00Z',null, true, false)
